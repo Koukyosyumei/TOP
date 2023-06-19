95268a34ba1d869cbcbe9805b79f19c0802dd0ab
Koukyosyumei <koukyosyumei@hotmail.com>
2023-06-19 17:40:11 +0900

    update

--- experiment.sh
+++ experiment.sh
@@ -10,12 +10,14 @@ for seed in 1 2 3 4 5
  done
 done
 
+
 for seed in 1 2 3 4 5
  do
  for n in 9 11 13
   do
   for e in 0.1 0.3
    do
+   FILE_NAME="random_n=${n}_e=${e}_s=${seed}_"
    ./klta -k 2 -h blind -j random -c < data/${FILE_NAME}.in > output/blind-random-${FILE_NAME}.out &
   done
  done
@@ -44,7 +46,6 @@ for seed in 1 2 3 4 5
   for e in 0.1 0.3
    do
    FILE_NAME="random_n=${n}_e=${e}_s=${seed}_"
-   python3 random_graph_generator.py -n $n -e $e -s $seed > data/${FILE_NAME}.in
    ./klta -k 2 -h singleton -j nearest -c -u < data/${FILE_NAME}.in > output/singleton-nearest-${FILE_NAME}.out &
   done
  done
