815d068d480a7d9676ac230cf5cf120cb7efd2bf
Koukyosyumei <koukyosyumei@hotmail.com>
2023-06-19 20:26:25 +0900

    update

--- geoexperiment.sh
+++ geoexperiment.sh
@@ -11,7 +11,7 @@ for k in 2 3 5 7
     for seed in 0 1 2 3 4
      do
      FILE_NAME="${k}-${el}-${h}-${j}_n=${368}_e=${851}_s=${seed}_"
-     ./klta -k $k -l ${el} -h ${h$ -j ${j} -u < data/map.in-${seed}.in > output/${FILE_NAME}k$k.out &
+     ./klta -k $k -l ${el} -h ${h} -j ${j} -u < data/map.in-${seed}.in > output/${FILE_NAME}k$k.out &
     done
     echo $k ${el} $h $j & wait
    done
