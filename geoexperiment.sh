548c14ec9987fbded5ac501d197151c2b9ee44e9
Koukyosyumei <koukyosyumei@hotmail.com>
2023-06-19 20:15:34 +0900

    update

---
+++ geoexperiment.sh
@@ -0,0 +1,22 @@
+python3 reuse.py -f assets/map.in -d data -s 42 -n 5
+
+for k in 2 3 5 7
+ do
+ for el in 0 100 300 500 700
+  do
+  for h in singleton
+   do
+   for j in random nearest
+    do
+    for seed in 0 1 2 3 4
+     do
+     FILE_NAME="${h}-${j}_n=${368}_e=${851}_s=${seed}_"
+     ./klta -k $k -l ${el} -h blind -j random -u < data/map.in-${seed}.in > output/${FILE_NAME}k$k.out &
+    done
+    echo $k ${el} $h $j & wait
+   done
+  done
+ done
+done
+
+
--- reuse.py
+++ reuse.py
@@ -1,9 +1,11 @@
 import argparse
 import random
+import os
 
 
 def add_args(parser):
     parser.add_argument("-f", "--file", type=str)
+    parser.add_argument("-d", "--output_dir", type=str)
     parser.add_argument("-s", "--seed", default=42, type=int)
     parser.add_argument("-n", "--num_outputs", default=1, type=int)
     args = parser.parse_args()
@@ -19,11 +21,13 @@ if __name__ == "__main__":
     with open(parsed_args.file, mode="r") as f:
         lines = f.readlines()
 
+    base_name = parsed_args.file.split("/")[-1]
     N, _ = tuple(map(int, lines[0][:-1].split(" ")))
 
     for i in range(parsed_args.num_outputs):
         source_goal = random.sample(list(range((N))), 2)
-        with open(parsed_args.file + "-" + str(i) + ".txt", 'w') as file:
+        with open(os.path.join(parsed_args.output_dir,
+                               base_name + "-" + str(i) + ".in"), 'w') as file:
             for line in lines[:-1]:
                 file.write(line)
             file.write(f"{source_goal[0]} {source_goal[1]}")
