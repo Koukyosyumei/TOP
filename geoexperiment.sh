b800479ecec5d91197dcafcfd6498f2ce865965e
Koukyosyumei <koukyosyumei@hotmail.com>
2023-06-20 11:36:51 +0900

    update

--- geoexperiment.sh
+++ geoexperiment.sh
@@ -1,6 +1,6 @@
 python3 reuse.py -f assets/map.in -d data -s 42 -n 5
 
-for k in 2 3 5 7
+for k in 2 3 5
  do
  for el in 0 50 100 300
   do
@@ -11,7 +11,7 @@ for k in 2 3 5 7
     for seed in 0 1 2 3 4
      do
      FILE_NAME="${k}-${el}-${h}-${j}_n=${368}_e=${851}_s=${seed}_"
-     ./klta -k $k -l ${el} -h ${h} -j ${j} -u < data/map.in-${seed}.in > output/${FILE_NAME}k$k.out &
+     ./klta -k $k -l ${el} -h ${h} -j ${j} -u < data/map.in-${seed}.in > output/${FILE_NAME}k$k.out
     done
     echo $k ${el} $h $j & wait
    done
