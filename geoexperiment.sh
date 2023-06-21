91e1a9781f2274ab5fb6e6e26541029963ae5644
Koukyosyumei <koukyosyumei@hotmail.com>
2023-06-21 21:13:09 +0900

    update

--- geoexperiment.sh
+++ geoexperiment.sh
@@ -4,16 +4,16 @@ for k in 2 3 5
  do
  for el in 0 50 100 300
   do
-  for h in singleton
+  for h in blind singleton
    do
-   for j in random nearest nearestdist
+   for j in random nearest
     do
     for seed in {0..9}
      do
      FILE_NAME="${k}-${el}-${h}-${j}_n=${368}_e=${851}_s=${seed}_"
-     ./klta -k $k -l ${el} -h ${h} -j ${j} < data/map.in-${seed}.in > output/${FILE_NAME}k$k.out &
+     ./klta -k ${k} -l ${el} -h ${h} -j ${j} < data/map.in-${seed}.in > output/${FILE_NAME}k${k}.out &
     done
-    echo $k ${el} $h $j & wait
+    echo ${k} ${el} ${h} ${j} & wait
    done
   done
  done
