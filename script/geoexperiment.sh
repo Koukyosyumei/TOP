9c6acbccaeb7a6a4083d591082069dde77d7decb
Koukyosyumei <koukyosyumei@hotmail.com>
2023-06-27 08:41:58 +0900

    update

--- script/geoexperiment.sh
+++ script/geoexperiment.sh
@@ -1,17 +1,17 @@
-python3 reuse.py -f assets/map.in -d data -s 42 -n 10
+python3 script/reuse.py -f assets/map.in -d data -s 42 -n 10
 
 for k in 2 3 5
  do
- for el in 0 50 100 300
+ for el in 0 100 300
   do
-  for h in singleton
+  for h in blind tunnel tunnel+
    do
    for j in random nearest
     do
     for seed in {0..9}
      do
      FILE_NAME="${k}-${el}-${h}-${j}_n=${368}_e=${851}_s=${seed}_"
-     ./klta -k ${k} -l ${el} -h ${h} -j ${j} < data/map.in-${seed}.in > output/${FILE_NAME}k${k}.out
+     ./topsolver -k ${k} -l ${el} -h ${h} -j ${j} -b 100 -t 10000 < data/map.in-${seed}.in > output/${FILE_NAME}k${k}.out
     done
     echo ${k} ${el} ${h} ${j} & wait
    done
@@ -19,5 +19,5 @@ for k in 2 3 5
  done
 done
 
-python3 report.py -d "output/*" -o georeport.csv
+python3 script/report.py -d "output/*" -o georeport_summary.csv -t georeport_time.csv
 
