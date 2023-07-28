ad67c9a562d29cf98b3f7fa6db57c33ec61a59cd
Koukyosyumei <koukyosyumei@hotmail.com>
2023-07-28 17:05:51 +0900

    update

--- script/approx.sh
+++ script/approx.sh
@@ -4,11 +4,11 @@ for seed in 1 2 3 4 5
  do
  for f in "lak102d.map" "lak108d.map" "lak109d.map" "den101d.map" "den201d.map" 
    do
-   python3 script/random_graph_generator.py -t fgrid -g assets/${f} -s $seed > data/${f}_${seed}.in
+   python3 script/random_graph_generator.py -t fgrid -g assets/${f} -s $seed > "data/${f}_n=1_e=1_s=${seed}.in"
  done
 done
 
-for k in 2 3 5
+for k in 2
  do
  for el in 1 2 3
   do
@@ -20,10 +20,12 @@ for k in 2 3 5
     do
      for seed in 1 2 3 4 5
       do
-     ./topsolver -k ${k} -l ${el} -h ${h} -j ${j} -b 100 -t 100000 -f output/${f}_${seed}.out -c -u < data/${f}_${seed}.in &
+      ./topsolver -k ${k} -l ${el} -h ${h} -j ${j} -b 100 -t 100000 -f output/${h}${j}${k}${el}${f}_${seed}.out -c -u < "data/${f}_n=1_e=1_s=${seed}.in" &
+      ./topsolver -k ${k} -l ${el} -h ${h} -j ${j} -b 100 -t 100000 -v radius -r 1 -f output/r1${h}${j}${k}${el}${f}_${seed}.out -c -u < "data/${f}_n=1_e=1_s=${seed}.in" &
+      ./topsolver -k ${k} -l ${el} -h ${h} -j ${j} -b 100 -t 100000 -v radius -r 2 -f output/r2${h}${j}${k}${el}${f}_${seed}.out -c -u < "data/${f}_n=1_e=1_s=${seed}.in" &
      done
+     echo ${f} ${k} ${el} ${h} ${j} & wait
     done
-    echo ${k} ${el} ${h} ${j} & wait
    done
   done
  done
--- script/report.py
+++ script/report.py
@@ -23,104 +23,107 @@ if __name__ == "__main__":
     rows = []
     df_times_logs = []
     for path in glob.glob(parsed_args.dir):
-        matches = re.findall(r"=(.*?)_", path)
-        gtype = "null"
-        n = -1
-        ep = -1
-        s = -1
-        if len(matches) >= 4:
-            gtype = matches[0]
-            n = matches[1]
-            ep = matches[2]
-            s = matches[3]
+        try:
+            matches = re.findall(r"=(.*?)_", path)
+            gtype = "null"
+            n = -1
+            ep = -1
+            s = -1
+            if len(matches) >= 4:
+                gtype = matches[0]
+                n = matches[1]
+                ep = matches[2]
+                s = matches[3]
 
-        with open(path, mode="r") as f:
-            lines = f.readlines()
-        lines = [lin.replace("\n", "") for lin in lines]
+            with open(path, mode="r") as f:
+                lines = f.readlines()
+            lines = [lin.replace("\n", "") for lin in lines]
 
-        if len(lines) < 10:
+            if len(lines) < 10:
+                continue
+
+            k = lines[1].split("=")[1]
+            el = lines[2].split("=")[1]
+            v = lines[3].split("=")[1]
+            h = lines[4].split("=")[1]
+            j = lines[5].split("=")[1]
+            c = lines[6].split("=")[1]
+            u = lines[7].split("=")[1]
+            p = lines[8].split("=")[1]
+            r = lines[9].split("=")[1]
+
+            times_log = []
+            for i, lin in enumerate(lines):
+                if "[ms]" in lin:
+                    infos = lin.split(", ")
+                    parsed_infos = [float(i.split(" ")[0]) for i in infos]
+                    times_log.append([path] + parsed_infos)
+                if "Performance Summary of DFBB" in lin:
+                    break
+
+            if len(lines) == i + 1:
+                continue
+
+            if len(times_log) > 0:
+                df_time_single = pd.DataFrame(times_log)
+                df_time_single.columns = [
+                    "path",
+                    "time",
+                    "NumberOfAnonymizedPaths",
+                    "AverageCost",
+                    "NumberOfPartitions",
+                    "NumberOfValidPartitions",
+                    "NumberofSkipped",
+                    "NumberOfDuplicated",
+                    "NumberOfNodes",
+                ]
+                df_times_logs.append(df_time_single)
+
+            num_evaluated_partitions = lines[i + 1].split(": ")[1]
+            num_valid_partitions = lines[i + 2].split(": ")[1]
+            num_skipped_partitions = lines[i + 3].split(": ")[1]
+            num_expanded_nodes = lines[i + 4].split(": ")[1]
+            num_evaluated_partitions_till_first = lines[i + 5].split(": ")[1]
+            num_expanded_nodes_till_first = lines[i + 6].split(": ")[1]
+            num_duplicated_partitions = lines[i + 7].split(": ")[1]
+            num_tot_paths = lines[i + 8].split(": ")[1]
+            num_anonymized_paths = lines[i + 9].split(": ")[1]
+            avg_cost_anonymized_paths = lines[i + 10].split(": ")[1]
+            total_time = lines[i + 11].split(": ")[1].split(" [ms]")[0]
+            lowerbound_cost = lines[i + 12].split(": ")[1]
+
+            rows.append(
+                [
+                    path,
+                    gtype,
+                    n,
+                    ep,
+                    s,
+                    k,
+                    el,
+                    v,
+                    h,
+                    j,
+                    c,
+                    u,
+                    p,
+                    r,
+                    num_evaluated_partitions,
+                    num_valid_partitions,
+                    num_skipped_partitions,
+                    num_expanded_nodes,
+                    num_tot_paths,
+                    num_anonymized_paths,
+                    num_evaluated_partitions_till_first,
+                    num_expanded_nodes_till_first,
+                    avg_cost_anonymized_paths,
+                    total_time,
+                    lowerbound_cost,
+                    num_duplicated_partitions,
+                ]
+            )
+        except:
             continue
-
-        k = lines[1].split("=")[1]
-        el = lines[2].split("=")[1]
-        v = lines[3].split("=")[1]
-        h = lines[4].split("=")[1]
-        j = lines[5].split("=")[1]
-        c = lines[6].split("=")[1]
-        u = lines[7].split("=")[1]
-        p = lines[8].split("=")[1]
-        r = lines[9].split("=")[1]
-
-        times_log = []
-        for i, lin in enumerate(lines):
-            if "[ms]" in lin:
-                infos = lin.split(", ")
-                parsed_infos = [float(i.split(" ")[0]) for i in infos]
-                times_log.append([path] + parsed_infos)
-            if "Performance Summary of DFBB" in lin:
-                break
-
-        if len(lines) == i + 1:
-            continue
-
-        if len(times_log) > 0:
-            df_time_single = pd.DataFrame(times_log)
-            df_time_single.columns = [
-                "path",
-                "time",
-                "NumberOfAnonymizedPaths",
-                "AverageCost",
-                "NumberOfPartitions",
-                "NumberOfValidPartitions",
-                "NumberofSkipped",
-                "NumberOfDuplicated",
-                "NumberOfNodes",
-            ]
-            df_times_logs.append(df_time_single)
-
-        num_evaluated_partitions = lines[i + 1].split(": ")[1]
-        num_valid_partitions = lines[i + 2].split(": ")[1]
-        num_skipped_partitions = lines[i + 3].split(": ")[1]
-        num_expanded_nodes = lines[i + 4].split(": ")[1]
-        num_evaluated_partitions_till_first = lines[i + 5].split(": ")[1]
-        num_expanded_nodes_till_first = lines[i + 6].split(": ")[1]
-        num_duplicated_partitions = lines[i + 7].split(": ")[1]
-        num_tot_paths = lines[i + 8].split(": ")[1]
-        num_anonymized_paths = lines[i + 9].split(": ")[1]
-        avg_cost_anonymized_paths = lines[i + 10].split(": ")[1]
-        total_time = lines[i + 11].split(": ")[1].split(" [ms]")[0]
-        lowerbound_cost = lines[i + 12].split(": ")[1]
-
-        rows.append(
-            [
-                path,
-                gtype,
-                n,
-                ep,
-                s,
-                k,
-                el,
-                v,
-                h,
-                j,
-                c,
-                u,
-                p,
-                r,
-                num_evaluated_partitions,
-                num_valid_partitions,
-                num_skipped_partitions,
-                num_expanded_nodes,
-                num_tot_paths,
-                num_anonymized_paths,
-                num_evaluated_partitions_till_first,
-                num_expanded_nodes_till_first,
-                avg_cost_anonymized_paths,
-                total_time,
-                lowerbound_cost,
-                num_duplicated_partitions,
-            ]
-        )
 
     df = pd.DataFrame(rows)
     df.columns = [
