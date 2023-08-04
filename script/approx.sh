d7b192d9e8c9169a740ea165424eb36fd34644b0
Koukyosyumei <koukyosyumei@hotmail.com>
2023-08-04 19:53:42 +0900

    update

--- script/approx.sh
+++ script/approx.sh
@@ -1,8 +1,8 @@
-VALUE_C=5
+VALUE_C=6
 
 while getopts c: OPT; do
   case $OPT in
-  "d")
+  "c")
     FLG_C="TRUE"
     VALUE_C="$OPTARG"
     ;;
@@ -31,18 +31,38 @@ for k in 2 3
   do
   for h in blind tunnel
    do
-   for j in random
+   for j in "random" "asccost" "deccost" "adacost+"
     do
     for f in "den101d.map" "den201d.map" "lak202d.map" "lak510d.map" "orz000d.map" "orz301d.map" 
     do
      for seed in 1 2 3 4 5
       do
-      ./topsolver -k ${k} -l ${el} -h ${h} -j ${j} -b 100 -t 100000 -f output/${h}${j}${k}${el}${f}_${seed}.out -c -u < "data/${f}_n=1_e=1_s=${seed}.in" &
-      ./topsolver -k ${k} -l ${el} -h ${h} -j ${j} -b 100 -t 100000 -v radius -r 1 -f output/r1${h}${j}${k}${el}${f}_${seed}.out -c -u < "data/${f}_n=1_e=1_s=${seed}.in" &
-      ./topsolver -k ${k} -l ${el} -h ${h} -j ${j} -b 100 -t 100000 -v radius -r 10 -f output/r10${h}${j}${k}${el}${f}_${seed}.out -c -u < "data/${f}_n=1_e=1_s=${seed}.in" &
+      ./topsolver -k ${k} -l ${el} -h ${h} -j ${j} -b 100 -t 100000 -f output/${h}${j}${k}${el}${f}_${seed}merge.out -c -u < "data/${f}_n=1_e=1_s=${seed}.in" &
+      ./topsolver -k ${k} -l ${el} -h ${h} -j ${j} -b 100 -t 100000 -v radius -r 1 -f output/r1${h}${j}${k}${el}${f}_${seed}merge.out -c -u < "data/${f}_n=1_e=1_s=${seed}.in" &
+      ./topsolver -k ${k} -l ${el} -h ${h} -j ${j} -b 100 -t 100000 -v radius -r 10 -f output/r10${h}${j}${k}${el}${f}_${seed}merge.out -c -u < "data/${f}_n=1_e=1_s=${seed}.in" &
      done
      echo ${f} ${k} ${el} ${h} ${j} & wait
     done
+   done
+  done
+ done
+done
+
+for k in 2 3
+ do
+ for el in 1 10
+  do
+  for h in blind tunnel
+   do
+   for f in "den101d.map" "den201d.map" "lak202d.map" "lak510d.map" "orz000d.map" "orz301d.map" 
+    do
+    for seed in 1 2 3 4 5
+     do
+     ./topsolver -k ${k} -l ${el} -h ${h} -p "df+" -b 100 -t 100000 -f output/${h}${k}${el}${f}_${seed}df.out -c -u < "data/${f}_n=1_e=1_s=${seed}.in" &
+     ./topsolver -k ${k} -l ${el} -h ${h} -p "df+" -b 100 -t 100000 -v radius -r 1 -f output/r1${h}${k}${el}${f}_${seed}df.out -c -u < "data/${f}_n=1_e=1_s=${seed}.in" &
+     ./topsolver -k ${k} -l ${el} -h ${h} -p "df+" -b 100 -t 100000 -v radius -r 10 -f output/r10${h}${k}${el}${f}_${seed}df.out -c -u < "data/${f}_n=1_e=1_s=${seed}.in" &
+     done
+    echo ${f} ${k} ${el} ${h} & wait
    done
   done
  done
