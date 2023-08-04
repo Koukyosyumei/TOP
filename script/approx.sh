5791d6ef4f7817dee1858a38fab4df66937cd134
Koukyosyumei <koukyosyumei@hotmail.com>
2023-08-04 13:21:08 +0900

    update

--- script/approx.sh
+++ script/approx.sh
@@ -1,28 +1,31 @@
 python3 script/reuse.py -f assets/map.in -d data -s 42 -n 10
 
+# lak202d: 182 x 159: 6240
+# lak510d: 253 x 287: 7713
+
 for seed in 1 2 3 4 5
  do
- for f in "den401d.map" "hrt201n.map" 
+ for f in "den101d.map" "den201d.map" "lak202d.map" "lak510d.map" "orz00d.map" "orz201d.map" 
    do
-   python3 script/random_graph_generator.py -t fgrid -g assets/${f} -s $seed > "data/${f}_n=1_e=1_s=${seed}.in"
+   python3 script/random_graph_generator.py -t fgrid -g assets/${f} -s $seed -c 10 > "data/${f}_n=1_e=1_s=${seed}.in"
  done
 done
 
-for k in 2
+for k in 2 3
  do
- for el in 1 2 3
+ for el in 1 10
   do
   for h in blind tunnel
    do
-   for j in random nearest
+   for j in random
     do
-    for f in "den401d.map" "hrt201n.map" 
+    for f in "den101d.map" "den201d.map" "lak202d.map" "lak510d.map" "orz00d.map" "orz201d.map" 
     do
      for seed in 1 2 3 4 5
       do
       ./topsolver -k ${k} -l ${el} -h ${h} -j ${j} -b 100 -t 100000 -f output/${h}${j}${k}${el}${f}_${seed}.out -c -u < "data/${f}_n=1_e=1_s=${seed}.in" &
-      # ./topsolver -k ${k} -l ${el} -h ${h} -j ${j} -b 100 -t 100000 -v radius -r 1 -f output/r1${h}${j}${k}${el}${f}_${seed}.out -c -u < "data/${f}_n=1_e=1_s=${seed}.in" &
-      # ./topsolver -k ${k} -l ${el} -h ${h} -j ${j} -b 100 -t 100000 -v radius -r 2 -f output/r2${h}${j}${k}${el}${f}_${seed}.out -c -u < "data/${f}_n=1_e=1_s=${seed}.in" &
+      ./topsolver -k ${k} -l ${el} -h ${h} -j ${j} -b 100 -t 100000 -v radius -r 1 -f output/r1${h}${j}${k}${el}${f}_${seed}.out -c -u < "data/${f}_n=1_e=1_s=${seed}.in" &
+      ./topsolver -k ${k} -l ${el} -h ${h} -j ${j} -b 100 -t 100000 -v radius -r 2 -f output/r2${h}${j}${k}${el}${f}_${seed}.out -c -u < "data/${f}_n=1_e=1_s=${seed}.in" &
      done
      echo ${f} ${k} ${el} ${h} ${j} & wait
     done
@@ -31,5 +34,5 @@ for k in 2
  done
 done
 
-python3 script/report.py -d "output/*" -o approx_summary.csv -t approx_time.csv
+python3 script/report.py -d "output/*" -o report_summary.csv -t report_time.csv
 
